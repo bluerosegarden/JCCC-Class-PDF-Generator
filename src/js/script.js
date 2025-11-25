12e104a381beef270fede28251a40dd57fb376a5
Blue Rose <mail@bluerose.garden>
2025-11-25 12:18:23 -0600

    add check for csv files and fix logic issue for uploading a single file

--- src/js/script.js
+++ src/js/script.js
@@ -26,60 +26,52 @@ const previewSvg = mainContent => {
         const cw = document.body.clientWidth - 200;
         svgElem.setAttribute('width', cw);
         svgElem.setAttribute('height', (height * cw) / width);
-      });
-    };
-
-    // Exports PDF and downloads it
-    const exportPdf = mainContent =>
-      $typst.pdf({ mainContent }).then(pdfData => {
-        var pdfFile = new Blob([pdfData], { type: 'application/pdf' });
-     pdfFile.name = "classes.pdf"
-
-        // Creates element with <a> tag
-        const link = document.createElement('a');
-        // Sets file content in the object URL
-        link.href = URL.createObjectURL(pdfFile);
-        // Sets file name
-        link.target = '_blank';
-        link.download = "classes.pdf"
-        // Triggers a click event to <a> tag to save file.
-        link.click();
-        //URL.revokeObjectURL(link.href);
-      });
-
-    /// Listens the 'load' event to initialize after loaded the bundle file from CDN (jsdelivr).
-    document.getElementById('typst').addEventListener('load', function () {
-      /// Initializes the Typst compiler and renderer. Since we use "all-in-one-lite.bundle.js" instead of
-      /// "all-in-one.bundle.js" we need to tell that the wasm module files can be loaded from CDN (jsdelivr).
-      $typst.setCompilerInitOptions({
-        getModule: () =>
-          'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler/pkg/typst_ts_web_compiler_bg.wasm',
-      });
-      $typst.setRendererInitOptions({
-        getModule: () =>
-          'https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer/pkg/typst_ts_renderer_bg.wasm',
-      });
-
-      /// Binds exportPdf action to the button
-      document.getElementById('export').addEventListener("click", processPDF);
-      /// Binds previewSvg action to the textarea
     });
+};
+
+// Exports PDF and downloads it
+const exportPdf = mainContent => $typst.pdf({ mainContent }).then(pdfData => {
+    var pdfFile = new Blob([pdfData], { type: 'application/pdf' });
+    pdfFile.name = "classes.pdf"
+
+    // Creates element with <a> tag
+    const link = document.createElement('a');
+    // Sets file content in the object URL
+    link.href = URL.createObjectURL(pdfFile);
+    // Sets file name
+    link.target = '_blank';
+    link.download = "classes.pdf"
+    // Triggers a click event to <a> tag to save file.
+    link.click();
+});
 
 
+//Processes all the files uploaded
 async function processFiles(){
-   const fileList = uploaded_files.files;
-   let csv_text = "";
+    const fileList = uploaded_files.files;
+    let csv_text = "";
 
-   csv_text += await fileList[0].text();
+    console.log(fileList)
+    if (fileList.length >= 1){
+        for (let i = 0; i < await fileList.length; i++){
+            console.log(`${i} loop`)
 
-   if (fileList.length > 1){
-     for (let i = 1; i < fileList.length; i++){
-       console.log(`${i} loop`)
-       let file_text = await fileList[i].text()
-       let split_file = file_text.split("\n");
-       split_file.splice(0,1);
-       let headerless_file = split_file.join("\n");
-       csv_text += "\n"+headerless_file;
+            let uploadedFile = await fileList[i]; 
+
+            let fileName = uploadedFile.name;
+            let fileExt = fileName.split(".").slice(-1)[0];
+            console.log(fileExt)
+
+            if (fileExt.toLowerCase() != "csv"){
+                alert("One or more of the files are not CSV!"); 
+                break;
+            }
+       
+            let file_text = uploadedFile.text()
+            let split_file = file_text.split("\n");
+            split_file.splice(0,1);
+            let headerless_file = split_file.join("\n");
+            csv_text += headerless_file+"\n";
      }
    }
    
